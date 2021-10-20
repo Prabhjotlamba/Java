e44e03ddde2fe5b69fed6beddbd2c72489d1dd6c
Prabhjot Kaur <92647813+Prabhjotlamba@users.noreply.github.com>
2021-10-20 15:25:00 +0530

    Create p2.java

---
+++ p2.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.lang.*;
+import java.util.* ;
+class substractionClass{
+    
+    public static int fun(int a, int b)
+    {
+        return a - b ;
+    }
+	public static void main (String[] args) {
+	    
+	    Scanner scn = new Scanner(System.in) ;
+	    
+	    int a , b ;
+	    a = scn.nextInt();
+	    b = scn.nextInt();
+	    
+	    System.out.println( fun(a , b) );
+	}
+}
